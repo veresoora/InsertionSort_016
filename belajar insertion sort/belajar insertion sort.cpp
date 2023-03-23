56926a7fa34cc1152a980f17d42df4533fcb2da4
veresoora <lahanabira@gmail.com>
2023-03-23 10:57:49 +0700

    Procedure input

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -3,3 +3,29 @@ using namespace std;
 
 int arr[20];			// Membuat Array dengan panjang data 20
 int n;					// Membuat variable inputan n
+
+void input() {			// Procedure Input
+	while (true) 
+	{	
+		cout << "Masukan banyaknya elemen pada array : ";		// Membuat Inputan jumlah element Array
+		cin >> n;												// memanggil variabel inputan n
+
+		if (n <= 20) {											// Membuat kondisi n tidak lebih dari 20 
+			break;
+		}
+		else 
+		{
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";	// Menampilkan Pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;														// Membuat jarak per baris program
+	cout << "====================" << endl;								// Membuat tampilan susunan data element array
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++)						// Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";		// Memasukkan atau menginputkan nilai data a 
+		cin >> arr[i];								// Menyimpan nilai data n kedalam array arr
+	}
+}
